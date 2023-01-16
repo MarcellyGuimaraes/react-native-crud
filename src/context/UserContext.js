2a2030f736fc24fe52b14ffaa344f07b251c5faa
MarcellyGuimaraes <souzaguimaraesmarcelly@gmail.com>
2023-01-16 17:41:17 -0300

    feat: Criação e implementação do provider

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -2,4 +2,16 @@ import { createContext } from 'react'
 
 const UserContext = createContext({})
 
+export const UserProvider = ({ children }) => {
+  return (
+    <UserContext.Provider
+      value={{
+        state: { users },
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  )
+}
+
 export default UserContext
